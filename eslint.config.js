2b66854e44c939531ad34707a0ba2f079c623a18
pirjoelbrecht-mizzion <pirjoelbrecht@gmail.com>
2026-01-05 08:39:24 +0000

    Updated eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,33 @@
 import react from "eslint-plugin-react";
 import hooks from "eslint-plugin-react-hooks";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
 
 export default [
   {
-    files: ["**/*.{js,jsx,ts,tsx}"],
+    files: ["**/*.{ts,tsx}"],
     ignores: ["node_modules", ".next", "dist", "build"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
     plugins: {
+      "@typescript-eslint": tsPlugin,
       react,
       "react-hooks": hooks,
     },
     rules: {
-      // Core sanity
-      "no-unused-vars": "warn",
+      // Turn off JS rule, use TS version instead
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["warn"],
 
-      // React Hooks (most important)
+      // Hooks rules (critical)
       "react-hooks/rules-of-hooks": "error",
       "react-hooks/exhaustive-deps": "warn",
     },
